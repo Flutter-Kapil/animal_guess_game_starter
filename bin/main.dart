8262fd45951fe83198a38bc24f05d2e132330e66
kapil jhajhria <jhajhria44@gmail.com>
2019-10-31 16:10:38 +0530

    start from scratch again save 2

--- bin/main.dart
+++ bin/main.dart
@@ -11,7 +11,9 @@ void main() {
   bool questionOn = true;
   Question question = Question();
   question.text = 'can it fly?';
+  question.yesNode = Question();
   question.yesNode.text = "duck";
+  question.noNode = Question();
   question.noNode.text = "monkey";
 
   print('lets play a question');
@@ -23,12 +25,46 @@ void main() {
     userInput = stdin.readLineSync();
     if (userInput == 'y') {
       if (question.yesNode.yesNode == null && question.yesNode.noNode == null) {
+        //this means that yesNode.text is animal name and its not a question
         print('is it ${question.yesNode.text}');
+        userInput = stdin.readLineSync();
+        if (userInput == 'y') {
+          exit(0);
+        } else if (userInput == 'n') {
+          helpMeImprove(question);
+          //TODO:help me improve here if answer is no , if its yes then exit
+        }
+      } else {
+        question = question.yesNode;
+        continue gameStart;
       }
     }
   }
 }
 
+void helpMeImprove(Question question) {
+  print('help me improve');
+  print('which animal were you thinking of?');
+  String newAnimal = stdin.readLineSync();
+  print(
+      'What question would distinguish between a ${question.yesNode.text} and A $newAnimal?');
+  String newQuestion = stdin.readLineSync();
+  print('how will you answer $newQuestion for  $newAnimal');
+  String userInput = stdin.readLineSync();
+  //--------------
+  String prevAnimal = question.yesNode.text;
+  question.yesNode.text = newQuestion;
+  question.yesNode.yesNode = Question();
+  question.yesNode.noNode = Question();
+  //---------
+  if (userInput == 'y') {
+    question.yesNode.yesNode.text = newAnimal;
+    question.yesNode.noNode.text = prevAnimal;
+  } else if (userInput == 'n') {
+    question.yesNode.yesNode.text = prevAnimal;
+    question.yesNode.noNode.text = newAnimal;
+  }
+}
 //void helpMeImprove(Question qBank) {
 //  print('help me improve');
 //  print('which animal were you thinking of?');

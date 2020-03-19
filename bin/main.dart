264e30d85d96ea05f78e272eb945637d5f0c32d3
kapiljhajhria <Aerospace44>
2020-03-19 09:45:49 +0530

    starting working, can't add new questions yet

---
+++ bin/main.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+class Node {
+  String que;
+  Node yes;
+  Node no;
+  Node({this.yes, this.no, this.que});
+
+  bool isLast() => yes == null && no == null;
+  void confirmAnimal() => print("is it $que");
+  void askQuestion() => print(que);
+  void startOver() => print("do you want to play again");
+}
+
+class Game {
+  Node startNode = Node(que: "duck");
+
+  void startGame() {
+    Node node = startNode;
+    print("lets play a game");
+    print("think of an animal");
+    bool askingQuestions = !node.isLast();
+    askQue:
+    while (askingQuestions) {
+      //print question, can it fly? can it swim? etc
+      node.askQuestion();
+      String res = stdin.readLineSync();
+      if (res == 'yes' || res == 'y' || res == 'Yes' || res == 'Y') {
+        //if answer s yes, then switch to yes branch and if answer is no, then switch to no branch
+        node = node.yes;
+      } else {
+        node = node.no;
+      }
+      if (node.isLast())
+        askingQuestions =
+            false; //if its last node then you need to confirm the animal instead of asking the questions again
+    }
+
+    node.confirmAnimal();
+    String animalConfirmation = stdin.readLineSync();
+
+    if (animalConfirmation == 'yes' ||
+        animalConfirmation == 'y' ||
+        animalConfirmation == 'Yes' ||
+        animalConfirmation == 'Y') {
+      //if answer s yes, then switch to yes branch and if answer is no, then switch to no branch
+      node.startOver();
+      String startAgain = stdin.readLineSync();
+      if (startAgain == 'yes' ||
+          startAgain == 'y' ||
+          startAgain == 'Yes' ||
+          startAgain == 'Y') {
+        //start the same again
+        Game game = Game();
+        game.startGame();
+      } else {
+        //means answer is no, so end the game
+        return;
+      }
+    }
+  }
+}
+
+void main() {
+  Game game = Game();
+  game.startGame();
+}
